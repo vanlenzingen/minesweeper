c31e0575bcb201a6863fd5b9170281d0645d9060
vanlenzingen <van_lenzingen@yahoo.de>
2021-09-03 12:13:38 +0200

    los gehts

---
+++ src/minesweeper1/Pair.java
@@ -0,0 +1,17 @@
+package minesweeper1;
+
+public class Pair<X, Y> {
+		public int X; 
+		public int Y; 
+		public Pair(X x, Y y) { 
+		    this.X = (int) x; 
+		    this.Y = (int) y; 
+		  } 
+		  
+	public int getX() {
+		return X;
+	}		  
+	public int getY() {
+		return Y;
+	}
+} 
---
+++ src/minesweeper1/minesweeperGame.java
@@ -0,0 +1,111 @@
+package minesweeper1;
+
+import java.util.Random;
+
+
+public class minesweeperGame {
+	static int rows = 15;
+	static int cols = 15;
+	static int mines = 10;
+	static Pair<Integer, Integer>[] minePositions;
+	
+	
+	private static void setMinePosition(int rows, int cols, int mines) {
+		for(int i=0;i<mines;i++) {
+			Random r = new Random();
+			int x = r.nextInt(rows);
+			int y = r.nextInt(cols);
+			
+			Pair<Integer, Integer> minePosition = new Pair<Integer, Integer>(x, y);
+			minePositions[i]= minePosition;
+		}
+	}
+	
+	public static int[][] field;
+	private Pair<Integer, Integer>[] neighbors;
+	
+	//TODO
+		static Object resetGame() {
+			setMinePosition(rows, cols, mines);
+			//setButtons
+			System.out.println("reset");
+			return null;
+		}
+		
+		static int[][] fillField(Pair<Integer, Integer> minePosition, int cols, int rows) {
+		// init field
+			for(int i=0;i<cols;i++) {
+				for(int j=0;j<rows;j++) {
+					field[i][j] = 0;
+				}
+			}
+			//fill with mines && values
+			for(int i=0;i<minePositions.length;i++) {
+				int x =minePositions[i].getX();
+				int y =minePositions[i].getY();
+				field[x][y]=-1;
+				
+				Pair<Integer, Integer>[] neighbors = getNeighborsIndex(x,y);	
+							
+			}
+			
+			return field;
+		}
+
+		public Pair<Integer, Integer>[] getNeighborsIndex(int col, int row){
+			neighbors = null;
+			
+			if(col == 0) {
+				//TODO
+			}
+			if (row == 0) {
+				//TODO
+			}
+			if (col == minesweeperGame.getCols()) {
+				//TODO
+			}
+			if(row == minesweeperGame.getRows()) {
+				//TODO
+			}
+			
+			neighbors[0] = new Pair<Integer,Integer>(col-1, row-1);
+			neighbors[1] = new Pair<Integer,Integer>(col, row-1);
+			neighbors[2] = new Pair<Integer,Integer>(col+1, row-1);
+			neighbors[3] = new Pair<Integer,Integer>(col-1, row);
+			neighbors[4] = new Pair<Integer,Integer>(col-1, row+1);
+			neighbors[5] = new Pair<Integer,Integer>(col+1, row-1);
+			neighbors[6] = new Pair<Integer,Integer>(col+1, row-1);
+			neighbors[7] = new Pair<Integer,Integer>(col+1, row);
+			
+			return neighbors;
+		}
+
+		public static int getRows() {
+			return rows;
+		}
+
+
+		public static void setRows(int rows) {
+			minesweeperGame.rows = rows;
+		}
+
+
+		public static int getCols() {
+			return cols;
+		}
+
+
+		public static void setCols(int cols) {
+			minesweeperGame.cols = cols;
+		}
+
+
+		public static int getMines() {
+			return mines;
+		}
+
+
+		public static void setMines(int mines) {
+			minesweeperGame.mines = mines;
+		}
+}
